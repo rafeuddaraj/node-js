2fece72cedbca6cf19836565995bd6e10e789e39
Rafe Uddaraj <rafeuddaraj2@gmail.com>
2025-07-09 16:40:56 +0600

    fix: build error

--- app/[lang]/page.tsx
+++ app/[lang]/page.tsx
@@ -1,9 +1,4 @@
-import LocalizedLink from "@/components/localized-link";
-import { buttonVariants } from "@/components/ui/button";
 import { getDictionary, LangProps } from "@/lib/dictionaries";
-import { page_routes } from "@/lib/routes-config";
-import { MoveUpRightIcon, TerminalSquareIcon } from "lucide-react";
-import Link from "next/link";
 
 export default async function Home({ params }: LangProps) {
   const { lang } = await params;
--- components/footer.tsx
+++ components/footer.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
 import { buttonVariants } from "./ui/button";
-import { CommandIcon, HeartIcon, TriangleIcon } from "lucide-react";
+import { HeartIcon, TriangleIcon } from "lucide-react";
 import { Dictionary } from "@/lib/dictionaries";
 
 export function Footer({ dict }: { dict: Dictionary }) {
+  console.log(dict);
+
   return (
     <footer className="border-t w-full h-16">
       <div className="container flex items-center sm:justify-between justify-center sm:gap-0 gap-4 h-full text-muted-foreground text-sm flex-wrap sm:py-0 py-3 max-sm:px-4">
